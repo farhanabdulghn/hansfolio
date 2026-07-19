3818687070b3e64619a9a2fdd0a77bdb91810a9a
Farhan Ab.G <farhannamikaze18@gmail.com>
2026-07-20 02:34:32 +0700

    refactor: migrate to ts

---
+++ tailwind.config.d.ts
@@ -0,0 +1,13 @@
+declare const _default: {
+    content: string[];
+    theme: {
+        extend: {
+            colors: {
+                primary: string;
+                secondary: string;
+            };
+        };
+    };
+    plugins: never[];
+};
+export default _default;
